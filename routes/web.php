0bb41c5e5d583d9a4a41451a7be694c429f142c3
vagrant <vagrant@homestead>
2018-05-02 10:02:20 +0000

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/about','StaticPagesController@about')->name('about');
 // 注册路由
 Route::get('/signup','UsersController@create')->name('signup');
 Route::resource('users','UsersController');
+
+Route::get('login','SessionController@create')->name('login');
+Route::post('login','SessionController@store')->name('login');
+Route::delete('logout','SessionController@destroy')->name('logout');
